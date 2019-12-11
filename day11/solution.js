97451e2da99081b573f3ce7b18999c197309423b
Jeremy Lizza <jeremyl@S03487-MBPR.local>
2019-12-11 11:17:15 -0500

    still need to unjank this

--- day11/solution.js
+++ day11/solution.js
@@ -1,10 +1,12 @@
 let program = [3,8,1005,8,319,1106,0,11,0,0,0,104,1,104,0,3,8,102,-1,8,10,1001,10,1,10,4,10,108,1,8,10,4,10,101,0,8,28,2,1105,12,10,1006,0,12,3,8,102,-1,8,10,101,1,10,10,4,10,1008,8,0,10,4,10,102,1,8,58,2,107,7,10,1006,0,38,2,1008,3,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,0,8,10,4,10,1001,8,0,90,3,8,1002,8,-1,10,101,1,10,10,4,10,108,0,8,10,4,10,101,0,8,112,1006,0,65,1,1103,1,10,1006,0,91,3,8,102,-1,8,10,101,1,10,10,4,10,108,1,8,10,4,10,101,0,8,144,1006,0,32,3,8,1002,8,-1,10,101,1,10,10,4,10,108,1,8,10,4,10,102,1,8,169,1,109,12,10,1006,0,96,1006,0,5,3,8,102,-1,8,10,1001,10,1,10,4,10,108,1,8,10,4,10,101,0,8,201,3,8,102,-1,8,10,1001,10,1,10,4,10,108,0,8,10,4,10,1001,8,0,223,1,4,9,10,2,8,5,10,1,3,4,10,3,8,1002,8,-1,10,1001,10,1,10,4,10,108,1,8,10,4,10,101,0,8,257,1,1,9,10,1006,0,87,3,8,102,-1,8,10,1001,10,1,10,4,10,1008,8,0,10,4,10,102,1,8,287,2,1105,20,10,1,1006,3,10,1,3,4,10,101,1,9,9,1007,9,1002,10,1005,10,15,99,109,641,104,0,104,1,21102,1,932972962600,1,21101,0,336,0,1106,0,440,21101,838483681940,0,1,21101,0,347,0,1106,0,440,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,3,10,104,0,104,1,3,10,104,0,104,0,3,10,104,0,104,1,21101,3375393987,0,1,21101,394,0,0,1105,1,440,21102,46174071847,1,1,21102,1,405,0,1106,0,440,3,10,104,0,104,0,3,10,104,0,104,0,21101,988648461076,0,1,21101,428,0,0,1106,0,440,21101,0,709580452200,1,21101,439,0,0,1105,1,440,99,109,2,22101,0,-1,1,21101,40,0,2,21102,1,471,3,21102,461,1,0,1106,0,504,109,-2,2106,0,0,0,1,0,0,1,109,2,3,10,204,-1,1001,466,467,482,4,0,1001,466,1,466,108,4,466,10,1006,10,498,1102,0,1,466,109,-2,2105,1,0,0,109,4,1202,-1,1,503,1207,-3,0,10,1006,10,521,21102,1,0,-3,22102,1,-3,1,21201,-2,0,2,21101,0,1,3,21102,540,1,0,1106,0,545,109,-4,2106,0,0,109,5,1207,-3,1,10,1006,10,568,2207,-4,-2,10,1006,10,568,22101,0,-4,-4,1105,1,636,22102,1,-4,1,21201,-3,-1,2,21202,-2,2,3,21102,1,587,0,1105,1,545,22101,0,1,-4,21102,1,1,-1,2207,-4,-2,10,1006,10,606,21101,0,0,-1,22202,-2,-1,-2,2107,0,-3,10,1006,10,628,21201,-1,0,1,21101,0,628,0,106,0,503,21202,-2,-1,-2,22201,-4,-2,-4,109,-5,2106,0,0];
-
+console.log(program.length);
 // provide 0 if the robot is over a black panel or 1 if the robot is over a white panel
 let programpointer = 0;
 let panels = new Map();
 //init with 0,0
 let loc = buildnewpanel(0,0);
+let relativebase = 0;
+
 loc.color = "#";
 let dirs = ["^",">","v","<"];
 panels.set(loc.key, loc);
@@ -19,7 +21,7 @@ let ymax = 0;
 
 while(true) {
     //run the program and get the output
-    let outs = runprog(robot.program, [robot.currentloc.color === "." ? 0 : 1], programpointer, 2);
+    let outs = runprog(robot.program, [robot.currentloc.color === "." ? 0 : 1], programpointer, 2, relativebase);
     if(outs.length !== 2) break;
     programpointer = outs[1].pointer;
         
@@ -32,6 +34,10 @@ while(true) {
 
     //0 turn left, 1 turn right
     let curindex = dirs.indexOf(robot.facing);
+    if(curindex === -1) {
+        console.log("UH OH");
+        break;
+    }
     if(outs[1].output === 0) {
         curindex--;
         if (curindex === -1) curindex = 3;
@@ -40,6 +46,7 @@ while(true) {
         if (curindex === 4) curindex = 0;
     }
     robot.facing = dirs[curindex];
+    relativebase = outs[1].relativebase;
 
     let point = {"x": robot.currentloc.x, "y":robot.currentloc.y};
     switch(robot.facing) {
@@ -95,14 +102,14 @@ function buildnewpanel(x,y) {
     return {"key":`${x},${y}`, "x":x, "y":y, "color":'.', "painted": false};
 }
 
-function runprog(program, inputs, programpointer, outs) {
+function runprog(program, inputs, programpointer, outs, relativebase) {
     let inputpointer = 0;
     let terminate = false;
-    let relativebase = 0;
+    //let relativebase = 0;
     let loops = 0;
     let rout = [];
     for(var i = programpointer; i < program.length && !terminate;) {
-        console.log(`${++loops}, ${i}`);
+        //console.log(`${++loops}, ${i}`);
         let instruction = program[i].toString();
         //ABCDE - DE is 2 digit Opscode, C is mode of 1st param, B mode of 2nd param, A mode 3rd param
         //add 0's until it is 5 digits
@@ -113,17 +120,19 @@ function runprog(program, inputs, programpointer, outs) {
         let param1mode = parseInt(instruction.substring(2,3),10);
         let param2mode = parseInt(instruction.substring(1,2),10);
         let param3mode = parseInt(instruction.substring(0,1),10);
-
+        console.log(`i:${i}, OpCode:${opscode}, pmode1:${param1mode}, pmode2:${param2mode}, pmode3:${param3mode}`);
+        
 
         switch(opscode) {
             case 1:
                 //Opcode 1 adds together numbers read from two positions and stores the result in a third position
-                param1 = program[++i];
+                //param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
                 sum = param1v + param2v;
-                param3 = program[++i];
+                param3 = accessinstruction(program, ++i);;
                 if(param3mode === 1) {
                     if(param3 >= program.length) growprogram(program, param3);
                     program[param3] = sum;
@@ -133,12 +142,12 @@ function runprog(program, inputs, programpointer, outs) {
                 break;
             case 2:
                 //Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
                 product = param1v * param2v;
-                param3 = program[++i];
+                param3 = accessinstruction(program, ++i);
                 if(param3mode === 1) {
                     if(param3 >= program.length) growprogram(program, param3);
                     program[param3] = product;
@@ -148,7 +157,7 @@ function runprog(program, inputs, programpointer, outs) {
                 break;
             case 3:
                 //Opcode 3 takes a single integer as input and saves it to the position given by its only parameter
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 switch(param1mode) {
                     case 0:
                         if(param1 >= program.length) growprogram(program, param1);
@@ -168,34 +177,36 @@ function runprog(program, inputs, programpointer, outs) {
                 param1 = program[++i];
                 param1v = getvalue(param1mode, param1, program, relativebase);
                 i++;
-                console.log({"output": param1v, "pointer": i});
-                rout.push({"output": param1v, "pointer": i});
+                //console.log({"output": param1v, "pointer": i});
+                rout.push({"output": param1v, "pointer": i, "relativebase": relativebase});
                 break;
             case 5:
                 //if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
                 if(param1v !== 0) i = param2v;
                 else i++;
+                if(i >= program.length) growprogram(program, i);
+                console.log(`i after 5: ${i}, param2m: ${param2mode}, param2: ${param2}, relativebase: ${relativebase}`, );
                 break;
             case 6:
                 //if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
                 if(param1v === 0) i = param2v;
                 else i++;
                 break;
             case 7:
                 //if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
-                param3 = program[++i];
+                param3 = accessinstruction(program, ++i);
                 if(param3mode === 1) {
                     if(param3 >= program.length) growprogram(program, param3);
                     program[param3] = param1v < param2v ? 1 : 0;
@@ -204,11 +215,11 @@ function runprog(program, inputs, programpointer, outs) {
                 i++;
                 break;
             case 8:
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
                 param1v = getvalue(param1mode, param1, program, relativebase);
-                param2 = program[++i];
+                param2 = accessinstruction(program, ++i);
                 param2v = getvalue(param2mode, param2, program, relativebase);
-                param3 = program[++i];
+                param3 = accessinstruction(program, ++i);
                 if(param3mode === 1) {
                     if(param3 >= program.length) growprogram(program, param3);
                     program[param3] = param1v === param2v ? 1 : 0; 
@@ -218,7 +229,9 @@ function runprog(program, inputs, programpointer, outs) {
                 break;
             case 9:
                 //opcode 9 adjusts the relative base by the value of its only parameter. The relative base increases (or decreases, if the value is negative) by the value of the parameter.
-                param1 = program[++i];
+                param1 = accessinstruction(program, ++i);
+                console.log(`i before 9: ${i}, param1m: ${param1mode}, param1: ${param1}, relativebase: ${relativebase}`);
+                
                 switch(param1mode) {
                     case 0:
                         if(param1 >= program.length) growprogram(program, param1);
@@ -233,12 +246,13 @@ function runprog(program, inputs, programpointer, outs) {
                         break;
                 }
                 i++;
+                console.log(`i after 9: ${i}, param1m: ${param1mode}, param1: ${param1}, relativebase: ${relativebase}`);
                 break;
             case 99:
                 terminate = true;
                 //console.log("HALT");
                 //rout.push({"output": "HALT", "pointer": i});
-                return {"output": "HALT", "pointer": i};
+                return {"output": "HALT", "pointer": i, "relativebase": relativebase};
                 break;
         }
         if(rout.length === outs) return rout;
@@ -253,11 +267,19 @@ function getvalue(parammode, param, program, relativebase) {
             if(param >= program.length) growprogram(program, param);
             return program[param];
         case 1:
+            //if(param >= program.length) growprogram(program, param);
             return param;
         case 2:
             if((relativebase + param) >= program.length) growprogram(program, (relativebase + param));
             return program[relativebase + param];
     }
+}
+
+function accessinstruction(program, index) {
+    if(index >= program.length) {
+        growprogram(program, index);
+    }
+    return program[index];
 }
 
 function getindex(parammode, param, program, relativebase) {
@@ -266,6 +288,7 @@ function getindex(parammode, param, program, relativebase) {
             if(param >= program.length) growprogram(program, param);
             return param;
         case 1:
+            if(param >= program.length) growprogram(program, param);
             return param;
         case 2:
             if((relativebase + param) >= program.length) growprogram(program, (relativebase + param));
@@ -274,8 +297,9 @@ function getindex(parammode, param, program, relativebase) {
 }
 
 function growprogram(program, newindex) {
-    for(var i = program.length; i <= newindex+10; i++) {
+    console.log(`growing to ${newindex}`);
+    for(var i = program.length; i <= newindex; i++) {
         program.push(0);
     }
-    return program;
+    //return program;
 }
