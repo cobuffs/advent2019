d9b13fed658cb7c20897e603d36d29cca5d52712
cobuffs <jlizza@gmail.com>
2019-12-12 10:31:48 -0500

    hate you day 2

--- day12/solution.js
+++ day12/solution.js
@@ -1,6 +1,10 @@
 let moons = [buildmoon("Io",1,2,-9), buildmoon("Europa",-1,-9,-4), buildmoon("Ganymede",17,6,8), buildmoon("Callisto",12,4,2)];
 //let moons = [buildmoon("Io",-1,0,2), buildmoon("Europa",2,-10,-7), buildmoon("Ganymede",4,-8,8), buildmoon("Callisto",3,5,-1)];
 //let previousposmap = new Map();
+let moon0map = new Map();
+let moon1map = new Map();
+let moon2map = new Map();
+let moon3map = new Map();
 
 for(var i = 0;i < moons.length; i++) {
     calcenergy(moons[i]);
@@ -8,11 +12,51 @@ for(var i = 0;i < moons.length; i++) {
 
 //sim 1000 times for p1
 let steps = 0;
-while(true) {
+while(steps < 6000000) {
     applygravity();
     applyvelocity();
     steps++;
+    
+    let moon0 = moons[0];
+    let moon1 = moons[1];
+    let moon2 = moons[2];
+    let moon3 = moons[3];
 
+    let moon0key = `${moon0.x},${moon0.y},${moon0.z}`;
+    let moon1key = `${moon1.x},${moon1.y},${moon1.z}`;
+    let moon2key = `${moon2.x},${moon2.y},${moon2.z}`;
+    let moon3key = `${moon3.x},${moon3.y},${moon3.z}`;
+
+    let moon0c = false;
+    let moon1c = false;
+    let moon2c = false;
+    let moon3c = false;
+
+    if(moon0map.has(moon0key)){
+        //91054
+        //console.log(`collision of 0 at ${steps}`);
+        moon0c = true;
+    } else moon0map.set(moon0key,true);
+
+    if(moon1map.has(moon1key)){
+        console.log(`collision of 1 at ${steps}`);
+        moon1c = true;
+    } else moon1map.set(moon1key,true);
+
+    if(moon2map.has(moon2key)){
+        //console.log(`collision of 2 at ${steps}`);
+        moon2c = true;
+    } else moon2map.set(moon2key,true);
+
+    if(moon3map.has(moon3key)){
+        //console.log(`collision of 3 at ${steps}`);
+        moon3c = true;
+    } else moon3map.set(moon3key,true);
+
+    if(moon0c && moon1c && moon2c && moon3c) {
+        console.log(steps);
+        break;
+    }
 }
 
 //console.log(calctotalenergy());
