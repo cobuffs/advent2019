26cfbe251799a5acdf1d53a80c4ad824f875f1bb
cobuffs <jlizza@gmail.com>
2020-11-29 19:19:27 -0500

    clean up

--- day16/solution.js
+++ day16/solution.js
@@ -1,22 +1,23 @@
 //650
 let input = "59731816011884092945351508129673371014862103878684944826017645844741545300230138932831133873839512146713127268759974246245502075014905070039532876129205215417851534077861438833829150700128859789264910166202535524896960863759734991379392200570075995540154404564759515739872348617947354357737896622983395480822393561314056840468397927687908512181180566958267371679145705350771757054349846320639601111983284494477902984330803048219450650034662420834263425046219982608792077128250835515865313986075722145069152768623913680721193045475863879571787112159970381407518157406924221437152946039000886837781446203456224983154446561285113664381711600293030463013";
 let input0 = input;
-let sample0 = "03036732577212944063491565474664";
-let sample02 = sample0;
+let sample = "03036732577212944063491565474664";
+let sample0 = sample;
 
-let offset = 5973181;
+
+
+let offset = 303673;
 
 let pattern = [0,1,0,-1];
 //actual input is input repeated 10k times :(
-for(var i = 0; i < 9999; i++){
-    input = input + input0;
+for(var i = 0; i < 9; i++){
+    sample = sample + sample0;
 }
 
-let inputarr = input.split('');
+let inputarr = sample.split('');
 inputarr = inputarr.map(v => parseInt(v,10));
 
-//run for 4 phases
-let phasestorun = 0;
+let phasestorun = 100;
 for(var i = 0; i < phasestorun; i++) {
     let phasearr = [];
     for(var j = 1; j <= inputarr.length; j++) {
@@ -33,11 +34,15 @@ for(var i = 0; i < phasestorun; i++) {
             sum += (element * pattern[(k+1)%pattern.length]);
         }
         phasearr.push(Math.floor(Math.abs(sum) % 10));
-        //console.log(row);
+        //if(j % 1000000 === 0) console.log(`Element ${j} completed!`);
+        console.log(row);
     }
+    console.log(`Phase complete! ${phasearr.join("")}`);
     inputarr = phasearr;
 }
-console.log(inputarr.slice(offset,8));
+console.log(inputarr.join(""));
+
+//39563579 is too low
 
 function generatepatter(elementn) {
     let newarr = [];
